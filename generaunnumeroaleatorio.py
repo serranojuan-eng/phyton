0e9fcd79abd5978fead8b6ea88c5a021b2b52efa
serranojuan-eng <serranojuan@olayista.com>
2025-11-07 16:16:20 -0500

    Update and rename ejercicio_11.py to generaunnumeroaleatorio.py

--- generaunnumeroaleatorio.py
+++ generaunnumeroaleatorio.py
@@ -1,4 +1,4 @@
-# Ejercicio 11: Generar un número aleatorio del 0 al 200 y aumentarlo en 30%
+# Generar un número aleatorio del 0 al 200 y aumentarlo en 30%
 import random
 num = random.randint(0, 200)
 aumentado = num * 1.30
