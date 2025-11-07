4542146ab52178aa385a6ab18619a62982b99ac4
serranojuan-eng <serranojuan@olayista.com>
2025-11-07 16:47:01 -0500

    Update and rename ejercicio_4.py to Calcular el promedio de tres números.py

--- Calcular el promedio de tres números.py
+++ Calcular el promedio de tres números.py
@@ -1,4 +1,4 @@
-# Ejercicio 4: Calcular el promedio de tres números
+# Calcular el promedio de tres números
 n1 = float(input("Introduce el primer número: "))
 n2 = float(input("Introduce el segundo número: "))
 n3 = float(input("Introduce el tercer número: "))
