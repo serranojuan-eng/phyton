848f194231830e5407f3354c0a97edc6e7119329
serranojuan-eng <serranojuan@olayista.com>
2025-11-07 16:17:04 -0500

    Update and rename ejercicio_12.py to numeroaleatoriode10al50.py

--- numeroaleatoriode10al50.py
+++ numeroaleatoriode10al50.py
@@ -1,4 +1,4 @@
-# Ejercicio 12: Generar un número aleatorio del 10 al 50 y disminuirlo en 15%
+# Generar un número aleatorio del 10 al 50 y disminuirlo en 15%
 import random
 num = random.randint(10, 50)
 disminuido = num * 0.85
