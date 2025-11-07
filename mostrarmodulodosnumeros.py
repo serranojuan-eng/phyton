3fa5327319b5c02ec2aa0e66b28e4116bda61b36
serranojuan-eng <serranojuan@olayista.com>
2025-11-07 16:28:13 -0500

    Update and rename ejercicio_15.py to mostrarmodulodosnumeros.py

--- mostrarmodulodosnumeros.py
+++ mostrarmodulodosnumeros.py
@@ -1,4 +1,4 @@
-# Ejercicio 15: Mostrar el módulo de dos números
+# Mostrar el módulo de dos números
 num1 = int(input("Introduce el primer número: "))
 num2 = int(input("Introduce el segundo número: "))
 modulo = num1 % num2
